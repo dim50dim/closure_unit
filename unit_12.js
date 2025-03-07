ec080479ec6274e18330881eb79f42ed436c7696
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-07 17:09:34 +0100

    task 5 - a bit confusing

--- unit_12.js
+++ unit_12.js
@@ -63,9 +63,9 @@ console.log(count4());
 // Task 5
 // Напишите функцию замыкание generatePassword, которая принимает аргумент - массив символов, и содержит в себе две функции. Первая функция randomInteger(min, max) возрвщает случайное целое число в диапазоне, а вторую функцию возвращает (делает return). Данная функция, которую возвращают должна возвращать случайный символ из массива переданного функции generatePassword в качестве аргумента. 
 
-// function generatePassword(arr) {
+function generatePassword(arr) {
 
-// }
+}
 
 // let password = generatePassword(['b', 'y', 'Y', 'b', 'z']);
 // console.log(password());
