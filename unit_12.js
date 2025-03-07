fe25a0b58ed6d7001f6ce688728a9f4a62ce2d04
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-07 17:06:24 +0100

    not difficult - althougth - a bit strange - have to return two times

--- unit_12.js
+++ unit_12.js
@@ -33,11 +33,16 @@
 // Task 3.
 // В скобках напишите код функции замыкания, которая создает переменную count = 0 и возвращает анонимную функцию, которая в свою очередь увеличивает count на 1 и возвращает его. 
 
-// let count3 = (function () {
-
-// })();
-// console.log(count3());
-// console.log(count3());
+let count3 = (function () {
+   let count = 0;
+     
+        return () => {
+           return  count++;
+        }
+    
+})();
+console.log(count3());
+console.log(count3());
 
 
 // Task 4
