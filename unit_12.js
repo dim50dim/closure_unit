2910545976879c8c3da1246bbba7a2ddebd4cada
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-07 17:08:04 +0100

    in this task , have to do almost the same thing , except - using argument as a variable

--- unit_12.js
+++ unit_12.js
@@ -48,13 +48,16 @@ console.log(count3());
 // Task 4
 // Напишите функцию замыкание createCount. Внутри функции создайте count равный переданному в createCount аргументу ( по умолчанию равен 0) и сделайте return функции, которая увеличивает count на 1 и возвращает его. 
 
-// function createCount(тут аргумент) {
-// };
+function createCount(count) {
+     return () => {
+        return  count += 15;
+     }
+};
 
-// let count4 = createCount(444);
+let count4 = createCount(444);
 
-// console.log(count4());
-// console.log(count4());
+console.log(count4());
+console.log(count4());
 
 
 // Task 5
